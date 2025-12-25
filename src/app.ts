17e67ad857b3fc08466132279a60cd372bbdeb68
Andreas Køhlert <ak.stud@int.kirklarsen.dk>
2025-12-25 13:58:26 +0100

    cors

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ export async function startServer() {
     origin: [
       'http://localhost:3000', 
       'http://localhost:4000', 
-      'https://strategifrontend.onrender.com/'
+      'https://strategifrontend.onrender.com'
     ],
     credentials: true,
     methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
